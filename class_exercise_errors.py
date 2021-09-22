5c8cbac1efe574e13b29eb21d2de639a1a30fb35
Maryam Nuru <maryamnuru@Maryams-MacBook-Pro.local>
2021-09-22 13:14:56 -0400

    Class exercise on raising errors

--- class_exercise_errors.py
+++ class_exercise_errors.py
@@ -1,21 +1,32 @@
 # a = "The sky is blue"
 # print(a)
 
-#for letter in a:
+# for letter in a:
 #    print(letter)
 
 def function_name():
-
     try:
-        int("hello5")
-    except ValueError:
-        str("hello5")
+        from my_math_calculator import sqrt
+    except ModuleNotFoundError:
+        from math import sqrt
 
-    x = str("hello5")
+    x = sqrt(4)
     print(x)
+
 
 def main():
     function_name()
+    from my_math_calculator import add_positive_integers
+    try:
+        x = add_positive_integers(2, 3)
+        print(x)
+    except ValueError:
+        print("Got value error")
+    # except TypeError:
+        # print("Got Type Error")
+    except:
+        print("All other errors")
+
 
 if __name__ == "__main__":
-    main
+    main()
